b41c50f2f0a82e0e67b7c6be26cd630233e88d7c
Hakata <literallyme098@gmail.com>
2026-02-05 21:00:52 +0545

    drawing grid function added

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,41 @@
+/*
+Turn based Grid game
+*/ 
 #include<iostream>
+#include<vector>
+#include<string>
 
+//constants
+
+constexpr int gridWidth = 10;
+constexpr int gridHeight = 10;
+
+
+//function prototypes
+void drawGrid(const std::vector<std::string>& grid);
 int main()
 {
+  std::vector<std::string> grid = 
+  {
+    "############",
+    "#....P.....#",
+    "#..........#",
+    "#..........#",
+    "#..........#",
+    "#..........#",
+    "#..........#",
+    "#..........#",
+    "#....E.....#",
+    "############"
+  };
+  drawGrid(grid);
   return 0;
+}
+
+void drawGrid(const std::vector<std::string>& grid)
+{
+  for (const std::string& row : grid)
+  {
+    std::cout << row<< std::endl;  
+  }
 }
