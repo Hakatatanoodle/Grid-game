b60cf5bc32c668cf4ec2b17dfbd67cd898e67290
Hakata <literallyme098@gmail.com>
2026-02-07 20:38:08 +0545

    add reinserting player logic

--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,8 @@ constexpr int gridHeight = 10;
 void drawGrid(const std::vector<std::string>& grid);
 int main()
 {
-  int playerX,playerY;
+  int playerX=-1,playerY=-1;
+  bool found=false;//flag to check if player is found or not
   std::vector<std::string> grid = 
   {
     "############",
@@ -30,9 +31,9 @@ int main()
     "#....E.....#",
     "############"
   };
-  drawGrid(grid);
   
-  for(int i = 0; i < grid.size();i++)
+  
+  for(int i = 0; i < grid.size() && !found;i++)
   {
     for(int j = 0; j < grid[i].size(); j++)//here i is the row  and i am trying to stop at the end of the string which is controlled by i 
     {
@@ -40,11 +41,29 @@ int main()
       {
         playerX  = j;
         playerY = i;
+        found = true;
+        break;
       }
     }
   }
-  grid[playerX][playerY]='.';
-  std::cout << "Player Position: " << playerX << "," << playerY << std::endl;
+  if(playerX == -1 || playerY  == -1)
+  {
+    std::cout << "Player coordinates error!" << std::endl;
+    return 0;
+  }
+  else
+  {
+    grid[playerY][playerX]='.';//substitute the player with empty space(.)
+    std::cout << "Player Position: " << playerX << "," << playerY << std::endl;
+  }
+
+  //print the grid after extracting the player
+  drawGrid(grid);
+  //print a blanck line before reinserting the player
+  std::cout << std::endl;
+  //reinserting the player (modify the vector and reprint the gird)
+  grid[playerY][playerX] = 'P';
+  drawGrid(grid);
   return 0;
 }
 
