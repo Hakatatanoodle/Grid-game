929ddbdd9280d090f03bcd0e415634032434f0dc
Hakata <literallyme098@gmail.com>
2026-02-07 20:29:36 +0545

    Player search and remove from grid

--- src/main.cpp
+++ src/main.cpp
@@ -11,10 +11,12 @@ constexpr int gridWidth = 10;
 constexpr int gridHeight = 10;
 
 
+
 //function prototypes
 void drawGrid(const std::vector<std::string>& grid);
 int main()
 {
+  int playerX,playerY;
   std::vector<std::string> grid = 
   {
     "############",
@@ -29,8 +31,23 @@ int main()
     "############"
   };
   drawGrid(grid);
+  
+  for(int i = 0; i < grid.size();i++)
+  {
+    for(int j = 0; j < grid[i].size(); j++)//here i is the row  and i am trying to stop at the end of the string which is controlled by i 
+    {
+      if(grid[i][j] == 'P')
+      {
+        playerX  = j;
+        playerY = i;
+      }
+    }
+  }
+  grid[playerX][playerY]='.';
+  std::cout << "Player Position: " << playerX << "," << playerY << std::endl;
   return 0;
 }
+
 
 void drawGrid(const std::vector<std::string>& grid)
 {
